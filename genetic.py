922513a5ab292d387602985efa903abb3386276f
jordannelson0 <78982012+jordannelson0@users.noreply.github.com>
2023-05-18 18:43:53 +0100

    files uploaded
    Find 2 python files and a very basic PDF explaining how the code works - should you need any help

---
+++ genetic.py
@@ -0,0 +1,73 @@
+﻿import random
+import statistics
+import time
+import sys
+
+from pw_AI.finalcode.guessPasswordTests import getFitness
+
+
+def generateParent(length, geneSet, getFitness):
+    genes = []
+    while len(genes) < length:
+        sampSize = min(length - len(genes), len(geneSet))
+        genes.extend(random.sample(geneSet, sampSize))
+    genes = ''.join(genes)
+    fitness = getFitness(genes)
+    return Chromosome(genes, fitness)
+
+
+def mutate(parent, geneSet, get_fitness):
+    index = random.randrange(0, len(parent.Genes))
+    chGenes = list(parent.Genes)
+    nGene, alternate = random.sample(geneSet, 2)
+    chGenes[index] = alternate \
+        if nGene == chGenes[index] \
+        else nGene
+    genes = ''.join(chGenes)
+    fitness = get_fitness(genes)
+    return Chromosome(genes, fitness)
+
+
+def getBest(getFitness, tgtLen, optFitness, geneSet, display):
+    random.seed()
+    bParent = generateParent(tgtLen, geneSet, getFitness)
+    display(bParent)
+    if bParent.Fitness >= optFitness:
+        return bParent
+    while True:
+        child = mutate(bParent, geneSet, getFitness)
+        if bParent.Fitness >= child.Fitness:
+            continue
+        display(child)
+        if child.Fitness >= optFitness:
+            return child
+        bParent = child
+
+
+class Chromosome:
+    Genes = None
+    Fitness = None
+
+    def __init__(self, genes, fitness):
+        self.Genes = genes
+        self.Fitness = fitness
+
+
+class Benchmark:
+    @staticmethod
+    def run(function):
+        timings = []
+        stdout = sys.stdout
+        for i in range(100):
+            sys.stdout = None
+            startTime = time.time()
+            function()
+            seconds = time.time() - startTime
+            sys.stdout = stdout
+            timings.append(seconds)
+            mean = statistics.mean(timings)
+            if i < 10 or i % 10 == 9:
+                print("{0} {1:3.2f} {2:3.2f}".format(
+                    1 + i, mean,
+                    statistics.stdev(timings, mean)
+                    if i > 1 else 0))
---
+++ guessPasswordTests.py
@@ -0,0 +1,56 @@
+﻿import datetime
+import genetic
+import unittest
+import random
+
+
+def getFitness(guess, target):
+    return sum(1 for expected, actual in zip(target, guess)
+               if expected == actual)
+
+
+def display(candidate, startTime):
+    timeDiff = datetime.datetime.now() - startTime
+    print("{0}\t{1}\t{2}".format(
+        candidate.Genes, candidate.Fitness, str(timeDiff)))
+
+
+class GuessPasswordTests(unittest.TestCase):
+    geneset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZöäüÖÄÜ0123456789!.,#*?§$£%&:;\\/`´'µ=-_<>^°@#$%{}[" \
+              "]()'\"~;:.<> "
+
+    def test_1(self):
+        target = "Password123"  # easy password
+        self.guess_password(target)
+
+    def test_2(self):
+        target = "23PfEFXr@AdCZY4gM=wLvbaEQ2EXZmgG"  # 32 digit password
+        self.guess_password(target)
+
+    def guessPassword(self, target):
+        startTime = datetime.datetime.now()
+
+        def fnGetFitness(genes):
+            return getFitness(genes, target)
+
+        def fnDisplay(candidate):
+            display(candidate, startTime)
+
+        optimalFitness = len(target)
+        best = genetic.get_best(fnGetFitness, len(target),
+                                optimalFitness, self.geneset, fnDisplay)
+        self.assertEqual(best.Genes, target)
+
+    def test_Random(self):
+        length = 150
+        target = ''.join(random.choice(self.geneset) for _ in
+                         range(length))
+
+        self.guess_password(target)
+
+    def test_benchmark(self):
+        genetic.Benchmark.run(lambda: self.test_Random())
+
+
+if __name__ == '__main__':
+    unittest.main()  # make sure the tests are ran
